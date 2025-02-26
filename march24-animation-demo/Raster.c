01ed8bd411118bb7e942375c15bfad34e954cc60
ayushjaiswal-aj <ayushjaiswalaj@outlook.com>
2025-02-27 02:25:41 +0530

    adds forrest view

--- march24-animation-demo/Raster.c
+++ march24-animation-demo/Raster.c
@@ -403,6 +403,9 @@ void display(void)
 	// clear openGL bufferes
 	glClear(GL_COLOR_BUFFER_BIT);
 
+	drawDenseForrest();
+	drawFrontTrees();
+
 	// swap the bufferes
 	SwapBuffers(ghdc); // win32 function
 }
