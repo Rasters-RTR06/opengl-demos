fb49f09fccb575ef7f404b655855ef2f8c2da493
bhagyashree-chavan <bhagya.chavan@gmail.com>
2025-03-18 14:01:02 -0500

    added resize functionality to shri-krishna

--- common/props/fullkrushna.c
+++ common/props/fullkrushna.c
@@ -5,6 +5,13 @@
 void KrishnaRath(float originx, float originy, float resize) 
 {
 
+    //set matrix to model view mode
+    glMatrixMode(GL_MODELVIEW);
+
+    //set it to identity matrix
+    glLoadIdentity();
+    glScalef(0.3f, 0.3f, 0.5f);	// x axis rotation
+
 // CROWN
 // Triangle 1
 glBegin(GL_TRIANGLES);
@@ -377,5 +384,10 @@ glVertex2f(-0.378, 0.660);
 glVertex2f(-0.417, 0.620);
 glEnd();
 
+glMatrixMode(GL_MODELVIEW);
+
+//set it to identity matrix
+glLoadIdentity();
+glScalef(1.0f, 1.0f, 1.0f);	// x axis rotation
 
 }
