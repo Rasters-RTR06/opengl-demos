033fa70f5089664ded450a8b9bca8262ce475f76
cashew69 <nikhilsathe32@gmail.com>
2025-03-22 16:36:35 +0530

    resize functionality

--- common/props/Slogan.c
+++ common/props/Slogan.c
@@ -1,190 +1,191 @@
-void drawText(int r, int g, int b)
+
+void drawText(float originx, float originy, float resize, int r, int g, int b)
 {
     // code
     glColor3ub (r, g, b);
 
     glBegin(GL_LINE_STRIP);
-    glVertex2f(-1.3, 0.05);
-    glVertex2f(-1.25, 0.1);
-    glVertex2f(-1.2, 0.05);
+    glVertex2f(originx + -1.3*resize, originy + 0.05*resize);
+    glVertex2f(originx + -1.25*resize, originy + 0.1*resize);
+    glVertex2f(originx + -1.2*resize, originy + 0.05*resize);
     glVertex2f(-1.25, 0);
-    glVertex2f(-1.2, -0.05);
-    glVertex2f(-1.25, -0.1);
-    glVertex2f(-1.3, -0.05);
+    glVertex2f(originx + -1.2*resize, originy + -0.05*resize);
+    glVertex2f(originx + -1.25*resize, originy + -0.1*resize);
+    glVertex2f(originx + -1.3*resize, originy + -0.05*resize);
     glEnd();
     
     glBegin(GL_LINE_STRIP);
-    glVertex2f(-1.25, 0.0);
-    glVertex2f(-1.15, 0.0);
+    glVertex2f(originx + -1.25*resize, originy + 0.0*resize);
+    glVertex2f(originx + -1.15*resize, originy + 0.0*resize);
     glEnd();
     
     glBegin(GL_LINE_STRIP);
-    glVertex2f(-1.15, 0.1);
-    glVertex2f(-1.15, -0.1);
+    glVertex2f(originx + -1.15*resize, originy + 0.1*resize);
+    glVertex2f(originx + -1.15*resize, originy + -0.1*resize);
     glEnd();
 
     glBegin(GL_LINE_STRIP);
-    glVertex2f(-1.05, 0.0);
-    glVertex2f(-1.05, -0.05);
-    glVertex2f(-1.1, 0.0);
-    glVertex2f(-0.95, 0.0);
+    glVertex2f(originx + -1.05*resize, originy + 0.0*resize);
+    glVertex2f(originx + -1.05*resize, originy + -0.05*resize);
+    glVertex2f(originx + -1.1*resize, originy + 0.0*resize);
+    glVertex2f(originx + -0.95*resize, originy + 0.0*resize);
     glEnd();
 
     glBegin(GL_LINE_STRIP);
-    glVertex2f(-0.95, 0.0);
-    glVertex2f(-1.05, -0.075);
-    glVertex2f(-1.0, -0.1);
-    glVertex2f(-1.01, -0.04);
+    glVertex2f(originx + -0.95*resize, originy + 0.0*resize);
+    glVertex2f(originx + -1.05*resize, originy + -0.075*resize);
+    glVertex2f(originx + -1.0*resize, originy + -0.1*resize);
+    glVertex2f(originx + -1.01*resize, originy + -0.04*resize);
     glEnd();
 
     glBegin(GL_LINE_STRIP);
-    glVertex2f(-0.95, 0.1);
-    glVertex2f(-0.95, -0.1);
+    glVertex2f(originx + -0.95*resize, originy + 0.1*resize);
+    glVertex2f(originx + -0.95*resize, originy + -0.1*resize);
     glEnd();
 
     glBegin(GL_LINE_STRIP);
-    glVertex2f(-0.75, 0.0);
-    glVertex2f(-0.8, 0.05);
-    glVertex2f(-0.85, 0.0);
-    glVertex2f(-0.8, -0.05);
-    glVertex2f(-0.75, 0.0);
-    glVertex2f(-0.7, 0.05);
-    glVertex2f(-0.65, 0.0);
-    glVertex2f(-0.7, -0.05);
+    glVertex2f(originx + -0.75*resize, originy + 0.0*resize);
+    glVertex2f(originx + -0.8*resize, originy + 0.05*resize);
+    glVertex2f(originx + -0.85*resize, originy + 0.0*resize);
+    glVertex2f(originx + -0.8*resize, originy + -0.05*resize);
+    glVertex2f(originx + -0.75*resize, originy + 0.0*resize);
+    glVertex2f(originx + -0.7*resize, originy + 0.05*resize);
+    glVertex2f(originx + -0.65*resize, originy + 0.0*resize);
+    glVertex2f(originx + -0.7*resize, originy + -0.05*resize);
     glEnd();
 
     glBegin(GL_LINE_STRIP);
-    glVertex2f(-0.75, 0.1);
-    glVertex2f(-0.75, -0.1);
+    glVertex2f(originx + -0.75*resize, originy + 0.1*resize);
+    glVertex2f(originx + -0.75*resize, originy + -0.1*resize);
     glEnd();
 
     glBegin(GL_LINE_STRIP);
-    glVertex2f(-0.6, 0.1);
-    glVertex2f(-0.6, -0.1);
+    glVertex2f(originx + -0.6*resize, originy + 0.1*resize);
+    glVertex2f(originx + -0.6*resize, originy + -0.1*resize);
     glEnd();
 
     glBegin(GL_LINE_STRIP);
-    glVertex2f(-0.4, -0.05);
-    glVertex2f(-0.375, -0.025);
-    glVertex2f(-0.4, 0.0);
-    glVertex2f(-0.45, 0.0);
-    glVertex2f(-0.5, 0.05);
-    glVertex2f(-0.45, 0.1);
-    glVertex2f(-0.4, 0.1);
-    glVertex2f(-0.35, 0.05);
-    glVertex2f(-0.4, 0.0);
-    glVertex2f(-0.45, 0.0);
-    glVertex2f(-0.5, -0.05);
-    glVertex2f(-0.45, -0.1);
-    glVertex2f(-0.4, -0.1);
+    glVertex2f(originx + -0.4*resize, originy + -0.05*resize);
+    glVertex2f(originx + -0.375*resize, originy + -0.025*resize);
+    glVertex2f(originx + -0.4*resize, originy + 0.0*resize);
+    glVertex2f(originx + -0.45*resize, originy + 0.0*resize);
+    glVertex2f(originx + -0.5*resize, originy + 0.05*resize);
+    glVertex2f(originx + -0.45*resize, originy + 0.1*resize);
+    glVertex2f(originx + -0.4*resize, originy + 0.1*resize);
+    glVertex2f(originx + -0.35*resize, originy + 0.05*resize);
+    glVertex2f(originx + -0.4*resize, originy + 0.0*resize);
+    glVertex2f(originx + -0.45*resize, originy + 0.0*resize);
+    glVertex2f(originx + -0.5*resize, originy + -0.05*resize);
+    glVertex2f(originx + -0.45*resize, originy + -0.1*resize);
+    glVertex2f(originx + -0.4*resize, originy + -0.1*resize);
     glEnd();
 
     glBegin(GL_LINE_STRIP);
-    glVertex2f(-0.3, 0.1);
-    glVertex2f(-0.25, 0.05);
-    glVertex2f(-0.3, -0.05);
-    glVertex2f(-0.25, -0.1);
+    glVertex2f(originx + -0.3*resize, originy + 0.1*resize);
+    glVertex2f(originx + -0.25*resize, originy + 0.05*resize);
+    glVertex2f(originx + -0.3*resize, originy + -0.05*resize);
+    glVertex2f(originx + -0.25*resize, originy + -0.1*resize);
     glEnd();
 
     glBegin(GL_LINE_STRIP);
-    glVertex2f(-0.1, 0.1);
-    glVertex2f(-0.1, 0.05);
-    glVertex2f(-0.15, 0.0);
-    glVertex2f(-0.1, -0.05);
-    glVertex2f(-0.05, 0.0);
-    glVertex2f(-0.05, -0.05);
-    glVertex2f(-0.1, -0.05);
-    glVertex2f(-0.1, -0.15);
+    glVertex2f(originx + -0.1*resize, originy + 0.1*resize);
+    glVertex2f(originx + -0.1*resize, originy + 0.05*resize);
+    glVertex2f(originx + -0.15*resize, originy + 0.0*resize);
+    glVertex2f(originx + -0.1*resize, originy + -0.05*resize);
+    glVertex2f(originx + -0.05*resize, originy + 0.0*resize);
+    glVertex2f(originx + -0.05*resize, originy + -0.05*resize);
+    glVertex2f(originx + -0.1*resize, originy + -0.05*resize);
+    glVertex2f(originx + -0.1*resize, originy + -0.15*resize);
     glEnd();
 
     glBegin(GL_LINE_STRIP);
-    glVertex2f(0.0, 0.1);
-    glVertex2f(0.0, -0.1);
+    glVertex2f(originx + 0.0*resize, originy + 0.1*resize);
+    glVertex2f(originx + 0.0*resize, originy + -0.1*resize);
     glEnd();
 
     glBegin(GL_LINE_STRIP);
-    glVertex2f(0.1, 0.0);
-    glVertex2f(0.1, -0.05);
-    glVertex2f(0.05, 0.0);
-    glVertex2f(0.1, 0.0);
-    glVertex2f(0.2, 0.0);
-    glVertex2f(0.2, 0.1);
-    glVertex2f(0.2, -0.1);
+    glVertex2f(originx + 0.1*resize, originy + 0.0*resize);
+    glVertex2f(originx + 0.1*resize, originy + -0.05*resize);
+    glVertex2f(originx + 0.05*resize, originy + 0.0*resize);
+    glVertex2f(originx + 0.1*resize, originy + 0.0*resize);
+    glVertex2f(originx + 0.2*resize, originy + 0.0*resize);
+    glVertex2f(originx + 0.2*resize, originy + 0.1*resize);
+    glVertex2f(originx + 0.2*resize, originy + -0.1*resize);
     glEnd();
 
     glBegin(GL_LINE_STRIP);
-    glVertex2f(0.25, 0.1);
-    glVertex2f(0.25, -0.1);
+    glVertex2f(originx + 0.25*resize, originy + 0.1*resize);
+    glVertex2f(originx + 0.25*resize, originy + -0.1*resize);
     glEnd();
 
     glBegin(GL_LINE_STRIP);
-    glVertex2f(0.45, 0.0);
-    glVertex2f(0.4, 0.05);
-    glVertex2f(0.35, 0.0);
-    glVertex2f(0.4, -0.05);
-    glVertex2f(0.45, 0.0);
-    glVertex2f(0.45, 0.1);
-    glVertex2f(0.45, -0.1);
+    glVertex2f(originx + 0.45*resize, originy + 0.0*resize);
+    glVertex2f(originx + 0.4*resize, originy + 0.05*resize);
+    glVertex2f(originx + 0.35*resize, originy + 0.0*resize);
+    glVertex2f(originx + 0.4*resize, originy + -0.05*resize);
+    glVertex2f(originx + 0.45*resize, originy + 0.0*resize);
+    glVertex2f(originx + 0.45*resize, originy + 0.1*resize);
+    glVertex2f(originx + 0.45*resize, originy + -0.1*resize);
     glEnd();
 
     glBegin(GL_LINE_STRIP);
-    glVertex2f(0.375, 0.025);
-    glVertex2f(0.425, -0.025);
+    glVertex2f(originx + 0.375*resize, originy + 0.025*resize);
+    glVertex2f(originx + 0.425*resize, originy + -0.025*resize);
     glEnd();
 
     glBegin(GL_LINE_STRIP);
-    glVertex2f(0.5, 0.05);
-    glVertex2f(0.55, 0.05);
-    glVertex2f(0.5, 0.0);
-    glVertex2f(0.55, -0.05);
-    glVertex2f(0.6, 0.0);
-    glVertex2f(0.6, 0.1);
-    glVertex2f(0.6, -0.1);
+    glVertex2f(originx + 0.5*resize, originy + 0.05*resize);
+    glVertex2f(originx + 0.55*resize, originy + 0.05*resize);
+    glVertex2f(originx + 0.5*resize, originy + 0.0*resize);
+    glVertex2f(originx + 0.55*resize, originy + -0.05*resize);
+    glVertex2f(originx + 0.6*resize, originy + 0.0*resize);
+    glVertex2f(originx + 0.6*resize, originy + 0.1*resize);
+    glVertex2f(originx + 0.6*resize, originy + -0.1*resize);
     glEnd();
 
     glBegin(GL_LINE_STRIP);
-    glVertex2f(0.65, 0.1);
-    glVertex2f(0.65, -0.1);
+    glVertex2f(originx + 0.65*resize, originy + 0.1*resize);
+    glVertex2f(originx + 0.65*resize, originy + -0.1*resize);
     glEnd();
 
     glBegin(GL_LINE_STRIP);
-    glVertex2f(0.7, 0.1);
-    glVertex2f(0.75, 0.05);
-    glVertex2f(0.7, 0.0);
-    glVertex2f(0.75, -0.05);
-    glVertex2f(0.8, 0.0);
-    glVertex2f(0.8, 0.1);
-    glVertex2f(0.8, -0.1);
+    glVertex2f(originx + 0.7*resize, originy + 0.1*resize);
+    glVertex2f(originx + 0.75*resize, originy + 0.05*resize);
+    glVertex2f(originx + 0.7*resize, originy + 0.0*resize);
+    glVertex2f(originx + 0.75*resize, originy + -0.05*resize);
+    glVertex2f(originx + 0.8*resize, originy + 0.0*resize);
+    glVertex2f(originx + 0.8*resize, originy + 0.1*resize);
+    glVertex2f(originx + 0.8*resize, originy + -0.1*resize);
     glEnd();
 
     glBegin(GL_LINE_STRIP);
-    glVertex2f(0.8, 0.1);
-    glVertex2f(0.7, 0.2);
+    glVertex2f(originx + 0.8*resize, originy + 0.1*resize);
+    glVertex2f(originx + 0.7*resize, originy + 0.2*resize);
     glEnd();
 
     glBegin(GL_LINE_STRIP);
-    glVertex2f(-1.2, 0.1);
-    glVertex2f(-0.9, 0.1);
+    glVertex2f(originx + -1.2*resize, originy + 0.1*resize);
+    glVertex2f(originx + -0.9*resize, originy + 0.1*resize);
     glEnd();
 
     glBegin(GL_LINE_STRIP);
-    glVertex2f(-0.8, 0.1);
-    glVertex2f(-0.55, 0.1);
+    glVertex2f(originx + -0.8*resize, originy + 0.1*resize);
+    glVertex2f(originx + -0.55*resize, originy + 0.1*resize);
     glEnd();
 
     glBegin(GL_LINE_STRIP);
-    glVertex2f(-0.5, 0.1);
-    glVertex2f(-0.225, 0.1);
+    glVertex2f(originx + -0.5*resize, originy + 0.1*resize);
+    glVertex2f(originx + -0.225*resize, originy + 0.1*resize);
     glEnd();
 
     glBegin(GL_LINE_STRIP);
-    glVertex2f(-0.15, 0.1);
-    glVertex2f(0.3, 0.1);
+    glVertex2f(originx + -0.15*resize, originy + 0.1*resize);
+    glVertex2f(originx + 0.3*resize, originy + 0.1*resize);
     glEnd();
 
     glBegin(GL_LINE_STRIP);
-    glVertex2f(0.4, 0.1);
-    glVertex2f(0.85, 0.1);
+    glVertex2f(originx + 0.4*resize, originy + 0.1*resize);
+    glVertex2f(originx + 0.85*resize, originy + 0.1*resize);
     glEnd();
 
 }
