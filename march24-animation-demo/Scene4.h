3098327019458cbede62f3ad4ed299bf6295eeb6
Vaibhav Ile <vaibhavile@gmail.com>
2025-03-23 02:25:25 +0530

    Updated Scene 4

--- march24-animation-demo/Scene4.h
+++ march24-animation-demo/Scene4.h
@@ -26,6 +26,7 @@ float scaleScene = 1.0f;
 float sceneOriginX = 0.0f;
 float sceneOriginY = 0.0f;
 
+BOOL flag = FALSE;
 
 void sceneFourRender()
 {
@@ -37,10 +38,50 @@ void sceneFourRender()
 
      drawRoom();
      drawArjun(arjunOriginX, arjunOriginY, 0.7f, 0);
+     drawEllipse((COLOR) {0.0f, 0.0f, 0.0f, 1.0f},
+               (ANGLE) {0.0f, 2 * PI, 0.0f},
+               (SCALING) {1.0f, 1.0f, 1.0f},
+               (TRANSLATION) {arjunOriginX - 0.53f, arjunOriginY - 0.5f, 0.0f},
+               (REFLECTION) {1, 1, 1},
+               (RADIUS) {0.15f, 0.1f, 0.0f},
+               0.0f);
+
      drawBheem(bheemOriginX, bheemOriginY, 0.7f, 0);
+     drawEllipse((COLOR) {0.0f, 0.0f, 0.0f, 1.0f},
+               (ANGLE) {0.0f, 2 * PI, 0.0f},
+               (SCALING) {1.0f, 1.0f, 1.0f},
+               (TRANSLATION) {bheemOriginX - 0.53f, bheemOriginY - 0.5f, 0.0f},
+               (REFLECTION) {1, 1, 1},
+               (RADIUS) {0.15f, 0.1f, 0.0f},
+               0.0f);
+
      drawSahadev(yudhishteerOriginX, yudhishteerOriginY, 0.7f, 0);
+     drawEllipse((COLOR) {0.0f, 0.0f, 0.0f, 1.0f},
+               (ANGLE) {0.0f, 2 * PI, 0.0f},
+               (SCALING) {1.0f, 1.0f, 1.0f},
+               (TRANSLATION) {yudhishteerOriginX - 0.53f, yudhishteerOriginY - 0.5f, 0.0f},
+               (REFLECTION) {1, 1, 1},
+               (RADIUS) {0.15f, 0.1f, 0.0f},
+               0.0f);
+
      drawSahadev(sahadevOriginX, sahadevOriginY, 0.7f, 0);
+     drawEllipse((COLOR) {0.0f, 0.0f, 0.0f, 1.0f},
+               (ANGLE) {0.0f, 2 * PI, 0.0f},
+               (SCALING) {1.0f, 1.0f, 1.0f},
+               (TRANSLATION) {sahadevOriginX - 0.53f, sahadevOriginY - 0.5f, 0.0f},
+               (REFLECTION) {1, 1, 1},
+               (RADIUS) {0.15f, 0.1f, 0.0f},
+               0.0f);
+
      drawNakul(nakulOriginX, nakulOriginY, 0.7f, 0);
+     drawEllipse((COLOR) {0.0f, 0.0f, 0.0f, 1.0f},
+               (ANGLE) {0.0f, 2 * PI, 0.0f},
+               (SCALING) {1.0f, 1.0f, 1.0f},
+               (TRANSLATION) {nakulOriginX - 0.53f, nakulOriginY - 0.5f, 0.0f},
+               (REFLECTION) {1, 1, 1},
+               (RADIUS) {0.15f, 0.1f, 0.0f},
+               0.0f);
+
      drawDraupadi(draupadiOriginX, draupadiOriginY, 0.7f, 2, 1);
 
      glLoadIdentity();
@@ -86,15 +127,43 @@ void sceneFourUpdate(UINT elapsedTime)
      {
           sceneOriginY += 0.015f;
      }
+     else if (elapsedTime >= 90 && elapsedTime <= 99)
+     {
+          sceneOriginY += 0.0017f;
+          sceneOriginX -= 0.0049f;
+     }
+
+     //   [FOCUS ON DROUPADI]
+     else if (elapsedTime == 100)
+     {
+          scaleScene += 0.4f;
+          sceneOriginX = -0.1f;
+          sceneOriginY = 0.25f;
+     }
+     
+     //   [FOCUS ON PANDAV]
+     else if (elapsedTime == 110)
+     {
+          sceneOriginX = -0.3f;
+          sceneOriginY = -0.2f;
+     }
+
+     //   [SCALE BACK]
+     else if(elapsedTime == 120)
+     {
+          sceneOriginX = -0.3f;
+          sceneOriginY = 0.0f;
+          scaleScene -= 0.9f;
+     }
 
      //   [MOVE DROUPADI BACKWARD]
-     // else if (elapsedTime >= 115 && elapsedTime <= 150)
-     // {
-     //      draupadiOriginX -= 0.002f;
-     //      sceneOriginX += 0.002f;
-     // }
-     // else if (elapsedTime >= 150 && elapsedTime <= 200)
-     // {
-     //      draupadiOriginX -= 0.002f;
-     // }
+     else if (elapsedTime >= 120 && elapsedTime <= 150)
+     {
+          draupadiOriginX -= 0.0016f;
+     }
+     else if (elapsedTime >= 150)
+     {
+          draupadiOriginX -= 0.0016f;
+          sceneOriginX += 0.001f;
+     }
 }
