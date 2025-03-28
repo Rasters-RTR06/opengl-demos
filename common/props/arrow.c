cd43679fa12c49fcbb048197f94f62a77e5ef2da
Vighnesh Nair <nairvighnesh74@gmail.com>
2025-03-28 21:19:50 +0530

    updated Arrow

--- common/props/arrow.c
+++ common/props/arrow.c
@@ -1,21 +1,24 @@
-glPushMatrix();
-glTranslatef(0.3f, -0.3f, 0.0f);
-glRotatef(400.0f, 0.0f, 0.0f, 1.0f);
-glColor3f(0.5f, 0.35f, 0.05f);
-glBegin(GL_LINES);
-glVertex2f(0.0f, 0.0f);
-glVertex2f(0.4f, 0.0f);
-glEnd();
-glColor3f(0.8f, 0.8f, 0.8f);
-glBegin(GL_TRIANGLES);
-glVertex2f(0.4f, 0.0f);
-glVertex2f(0.35f, 0.05f);
-glVertex2f(0.35f, -0.05f);
-glEnd();
-glColor3f(1.0f, 0.0f, 0.0f);
-glBegin(GL_TRIANGLES);
-glVertex2f(0.0f, 0.0f);
-glVertex2f(0.05f, 0.03f);
-glVertex2f(0.05f, -0.03f);
-glEnd();
-glPopMatrix();
+void drawArrow(void)
+{
+    glPushMatrix();
+    glTranslatef(0.3f, -0.3f, 0.0f);
+    glRotatef(400.0f, 0.0f, 0.0f, 1.0f);
+    glColor3f(0.5f, 0.35f, 0.05f);
+    glBegin(GL_LINES);
+    glVertex2f(0.0f, 0.0f);
+    glVertex2f(0.4f, 0.0f);
+    glEnd();
+    glColor3f(0.8f, 0.8f, 0.8f);
+    glBegin(GL_TRIANGLES);
+    glVertex2f(0.4f, 0.0f);
+    glVertex2f(0.35f, 0.05f);
+    glVertex2f(0.35f, -0.05f);
+    glEnd();
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glBegin(GL_TRIANGLES);
+    glVertex2f(0.0f, 0.0f);
+    glVertex2f(0.05f, 0.03f);
+    glVertex2f(0.05f, -0.03f);
+    glEnd();
+    glPopMatrix();
+}
